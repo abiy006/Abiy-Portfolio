41887b9258ef7513cbe05659a76c17c07e888520
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-04-11 21:45:37 +0300

    linter is configured

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-const burger_menu = document.getElementById('mobileMenuid');
-const mobile_menu = document.getElementById("mobile-menu-page-id");
+const burgerMenu = document.getElementById('mobileMenuid');
+const mobileMenu = document.getElementById('mobile-menu-page-id');
 
-const mobile_landing = document.getElementById('mobile-landing-id');
-const mobile_menu_close = document.getElementById('mobile-menu-close-id');
-const mobile_menu_list_li = document.getElementById('moblie-menu-list-ul').querySelectorAll('li')
+const mobileLanding = document.getElementById('mobile-landing-id');
+const mobileMenuClose = document.getElementById('mobile-menu-close-id');
+const mobileMenuListLi = document.getElementById('moblie-menu-list-ul').querySelectorAll('li');
 
 const header = document.getElementById('header');
 
-mobile_menu.style.display = 'none';
-
-burger_menu.addEventListener('click', openMobilemenu);
-mobile_menu_close.addEventListener('click', mobileMenuclose);
-
-mobile_menu_list_li.forEach(listitems => {
-  listitems.addEventListener('click', mobileMenuclose)
-  })
+mobileMenu.style.display = 'none';
 
 function openMobilemenu() {
-  mobile_landing.style.backgroundColor = '#6070ff';
-  mobile_landing.classList.add('blur');
+  mobileLanding.style.backgroundColor = '#6070ff';
+  mobileLanding.classList.add('blur');
   header.style.backgroundColor = '#6070ff';
   header.classList.add('blur');
-  mobile_menu.style.display = 'block';
+  mobileMenu.style.display = 'block';
 }
 function mobileMenuclose() {
-  mobile_menu.style.display = 'none';
+  mobileMenu.style.display = 'none';
   header.style.backgroundColor = 'white';
-  mobile_landing.style.backgroundColor = 'white';
-  mobile_landing.classList.remove('blur');
+  mobileLanding.style.backgroundColor = 'white';
+  mobileLanding.classList.remove('blur');
   header.classList.remove('blur');
 }
+
+burgerMenu.addEventListener('click', openMobilemenu);
+mobileMenuClose.addEventListener('click', mobileMenuclose);
+
+mobileMenuListLi.forEach((listitems) => {
+  listitems.addEventListener('click', mobileMenuclose);
+});
