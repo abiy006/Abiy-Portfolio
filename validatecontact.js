45f9edaabdf208256bae3b9b6975c641bd9da272
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-04-15 17:11:15 +0300

    Email validation is started

--- validatecontact.js
+++ validatecontact.js
@@ -1,2 +1,30 @@
-const form = document.querySelector('form')
-console.log(form)
+const contactForm = document.getElementById('contactMe');
+
+const emailInputValidator = /^[a-z0-9_.+-]+@[a-z-]+\.[a-z0-9-.]+$/;
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const message = document.getElementById('respMessage');
+  const data = new FormData(e.target);
+  const email = data.get('email');
+
+  if (!emailInputValidator.test(email)) {
+    message.innerHTML = '* Error: Email field should be lower case';
+    return;
+  }
+
+  fetch(e.target.action, {
+    method: 'POST',
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then(() => {
+      message.innerHTML = '* Success: Thanks for contacting me!';
+    });
+
+  contactForm.reset();
+};
+contactForm.addEventListener('submit', onSubmit);
+
