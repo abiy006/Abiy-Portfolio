935201054b1d68e6cbf7c21ade213a153b07e04e
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-04-16 00:53:15 +0300

    Email checker fixed

--- JS/validatecontact.js
+++ JS/validatecontact.js
@@ -1,17 +1,22 @@
-const contactForm = document.getElementById('contactMe');
+const contactForm1 = document.getElementById('contactMeMob');
+const contactForm2 = document.getElementById('contactMeDesk');
 
 const emailInputValidator = /^[a-z0-9_.+-]+@[a-z-]+\.[a-z0-9-.]+$/;
 
 const onSubmit = (e) => {
   e.preventDefault();
-  const message = document.getElementById('response');
+  const message1 = document.getElementById('response-mob');
+  const message2 = document.getElementById('response-desk');
   const data = new FormData(e.target);
   const email = data.get('email');
 
   if (!emailInputValidator.test(email)) {
-    message.innerHTML = '* Error: Email field should be lower case';
-    message.style.color = '#602';
-    message.style.marginTop = '1rem';
+    message1.innerHTML = '* Error: Email field should be lower case';
+    message1.style.color = '#602';
+    message1.style.marginTop = '1rem';
+    message2.innerHTML = '* Error: Email field should be lower case';
+    message2.style.color = '#602';
+    message2.style.marginTop = '1rem';
     return;
   }
 
@@ -23,11 +28,16 @@ const onSubmit = (e) => {
     },
   })
     .then(() => {
-      message.innerHTML = '* Success: Thanks for contacting me!';
-      message.style.color = 'greenyellow';
-      message.style.marginTop = '1rem';
+      message1.innerHTML = '* Success: Thanks for contacting me!';
+      message1.style.color = 'greenyellow';
+      message1.style.marginTop = '1rem';
+      message2.innerHTML = '* Success: Thanks for contacting me!';
+      message2.style.color = 'greenyellow';
+      message2.style.marginTop = '1rem';
     });
 
-  contactForm.reset();
+    contactForm1.reset();
+    contactForm2.reset();
 };
-contactForm.addEventListener('submit', onSubmit);
+contactForm1.addEventListener('submit', onSubmit);
+contactForm2.addEventListener('submit', onSubmit);
