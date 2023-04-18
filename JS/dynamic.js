e51a1a4816dc233deb5b07b66bd4bb7d3160b7a4
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-04-18 22:48:21 +0300

    Checking Data

--- JS/dynamic.js
+++ JS/dynamic.js
@@ -40,12 +40,20 @@ const projectsArray = [
     liveVersion: 'https://abiy006.github.io/',
     source: 'https://github.com/abiy006/Abiy-Portfolio',
   },
+  {
+    id: 5,
+    prjectName: 'Multi-Post Stories',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    projectImg: 'Multi-PostStory1.svg',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    liveVersion: 'https://abiy006.github.io/',
+    source: 'https://github.com/abiy006/Abiy-Portfolio',
+  },
 ];
 
 function cardImage(imageParentDiv, imagePath) {
   const cardImage = document.createElement('img');
   cardImage.setAttribute('class', 'cardImageClass');
-  // cardImage.src = '../Images/Tonic1-pic.svg';
   cardImage.src = `./Images/${imagePath}`;
   cardImage.style.backgroundRepeat = 'no-repeat';
   cardImage.style.backgroundSize = 'contain';
